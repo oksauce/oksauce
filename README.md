1a4234d0cc7a5666c708213913c22274c690ed55
glyn thomas <glynthoma@gmail.com>
2017-10-17 17:19:47 +0100

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# oksauce
-OK Sauce Website 
---
+++ js/deferrals.js
@@ -0,0 +1,1 @@
+/* DEFER IFRAMES */ function init(){for(var t=document.getElementsByTagName("iframe"),e=0;e<t.length;e++)t[e].getAttribute("data-src")&&t[e].setAttribute("src",t[e].getAttribute("data-src"))}window.onload=init
