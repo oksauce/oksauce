76fdc9a3432575a7d8b37862eb45d35e2fd0071e
Glyn Thomas <mrglynthomas@gmail.com>
2019-01-19 17:13:40 +0000

    update

--- assets/js/preloadmain.js
+++ assets/js/preloadmain.js
@@ -28,7 +28,7 @@ window.onbeforeunload = function(e) {
 
 var bckimg = new Array('vertical-vinyl-1.jpeg');
 
-var topimg = new Array('oksauce-blk-30x25.png','oksauce-red-30x25.png');
+var topimg = new Array('oksauce-wht-30x25.png','oksauce-red-30x25.png');
 
 var news = new Array('blackhead','orangehead','greenhead','purplehead','redhead');
 
@@ -43,7 +43,7 @@ $(window).load(function() {
   var topdom = topimg[Math.floor(Math.random() * topimg.length)];
       topdom = 'assets/img/' + topdom + ')';
       $('#topimg').attr('src','/'+ topdom );
-      SetTop('oksauce-blk-30x25.png');
+      SetTop('oksauce-wht-30x25.png');
       setInterval(function() {SetTop();}, 10000);
 
   var newdom = news[Math.floor(Math.random() * news.length)];
@@ -54,7 +54,7 @@ $(window).load(function() {
 
     function SetTop(n) {
         var topdom = topimg[Math.floor(Math.random() * topimg.length)];
-        if( n == 'oksauce-blk-30x25.png') { topdom = 'oksauce-blk-30x25.png'};
+        if( n == 'oksauce-wht-30x25.png') { topdom = 'oksauce-wht-30x25.png'};
         topdom = 'assets/img/' + topdom;
         // setTimeout(function () {
         $('#topimg').attr('src','/'+ topdom);
