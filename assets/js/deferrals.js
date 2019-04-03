87b7c6a824954a3ff11397d21057e1da24a62dbf
Glyn Thomas <glynthomas@users.noreply.github.com>
2019-04-03 20:05:26 +0100

    Delete deferrals.js

--- assets/js/deferrals.js
+++
@@ -1,1 +0,0 @@
-/* DEFER IFRAMES */ function init(){for(var t=document.getElementsByTagName("iframe"),e=0;e<t.length;e++)t[e].getAttribute("data-src")&&t[e].setAttribute("src",t[e].getAttribute("data-src"))}window.onload=init
