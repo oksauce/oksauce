6def6d1d241f85b220f076fe962d73cf4552e5e2
Glyn Thomas <glynthomas@users.noreply.github.com>
2019-12-25 13:25:17 +0000

    Add files via upload

--- js/circuitous.js
+++ js/circuitous.js
@@ -1,17 +1,19 @@
 
-
 $.getJSON('/☰/circuitous.json').done(function(data){
   window.circuitous = data;
-  //console.log(window.questionnaire);
+
   RandomCircuitous();
   CountCircuitous();
   CheckCookie();
+
   console.log(isMobile);
   if (isMobile == true) {$("#circuitous").removeAttr('onclick')}
 });
 
 var isMobile = false; //initiate as false
 var obj_key_count = 0;
+var interval = null;
+
 // device detection
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
@@ -73,8 +75,8 @@ function InfoCircuitous(){
   window.ran_key_select = obj_keys[obj_key_count];
   window.selectedcircuitous = window.circuitousinfo[ran_key_select];
 
-  console.log(Object.keys(window.circuitousinfo).length);
-  console.log(obj_key_count);
+  //console.log(Object.keys(window.circuitousinfo).length);
+  //console.log(obj_key_count);
 
   if (obj_key_count != (Object.keys(window.circuitousinfo).length)) {    
       window.selectedcircuitous = window.circuitousinfo[ran_key_select];
@@ -88,7 +90,7 @@ function InfoCircuitous(){
   }
 
   window.selectedcircuitous = window.circuitousinfo[ran_key_select];
-  console.log(window.ran_key_select);
+  //console.log(window.ran_key_select);
   document.getElementById("circuitous").innerHTML = (JSON.stringify(window.selectedcircuitous.circuitousinfo)).replace(/\"/g, "").replace(/\"/g, "");
   document.getElementById("cowabunga").innerHTML = window.ran_key_select;
   console.info(ran_key_select + ' ' + JSON.stringify(window.selectedcircuitous.circuitousinfo).replace(/\"/g, "").replace(/\"/g, ""));
@@ -115,13 +117,20 @@ function CountCircuitous(){
 }
 
 function CheckCookie(){
-  if (getCookie("darkmode")=="" || getCookie("darkmode")=="disable")
-     {setCookie ("darkmode","disable",365);
-      cardColorDefault();
-  } else {
-      getCookie("darkmode")=="enable";
+
+  if (getCookie("darkmode")=="enable") {
       cardColor('rgb(36, 36, 38)');
+  //} else if (getCookie("darkmode")=="disable") {setCookie ("darkmode","disable",365);cardColorDefault();
+  } else if (getCookie("darkmode")=="auto") {
+      setCookie ("darkmode","auto",365);
+  } else if (getCookie("darkmode")=="notset") {
+  	  cardColorDefault();
   }
+/*
+  if (getCookie("darkmode")=="enable" || getCookie("darkmode")=="disable"){
+      setCookie ("darkmode","disable",365);cardColorDefault();
+  } else {getCookie("darkmode")=="enable";cardColor('rgb(36, 36, 38)');}
+*/
 }
 
 function createMailtoLinks(){
@@ -167,6 +176,8 @@ function clickCard(){
 
   $("#circuitous").attr("onclick","pickCircuitousInfo()");
   pickCircuitousInfo();
+
+  //if (getCookie("darkmode")=="disable") {setCookie ("darkmode","auto",365)}
   //$("#circuitous").html("inspired by Brian Eno and Peter Schmidt,circuitous cards are developing to inspire and build new processes for creative electronic music production");
 }
 
@@ -185,7 +196,8 @@ function cardColor(backcolor){
       $("#circuitous p").css("color", "rgba(255, 255, 255,255)");
       $("#footer a").css("color", "rgba(255, 255, 255, 255)");
 
-      setCookie ("darkmode","enabled",365);
+      setCookie ("darkmode","enable",365);
+      clearInterval(interval); // stop the interval
 
   } else if ($("#cardcolor").css('background-color') == backcolor.replace(/'/g, '"')) {
       $("#cardcolor").css("background-color", "rgba(0, 0, 0, 0)");
@@ -196,7 +208,10 @@ function cardColor(backcolor){
       $("#circuitous p").css("color", "rgba(0, 0, 0, 1)");
       $("#footer a").css("color", "rgba(0, 0, 0, 1)");
 
-      setCookie ("darkmode","disable",365);
+      //setCookie ("darkmode","disable",365);
+      setCookie ("darkmode","auto",365);
+      interval = setInterval(time, 3000);
+
   }
 
 }
@@ -204,10 +219,19 @@ function cardColor(backcolor){
 function cardColorDefault() {
 
   $("#cardcolor").css("background-color", "rgba(0, 0, 0, 0)");
-  //$("#cowabunger").css("color", "rgba(0, 0, 0, 0)");
   $("#header").css("color", "rgba(0, 0, 0, 1)");
   $("#circuitous").css("color", "rgba(0, 0, 0, 1)");
   $("#footer a").css("color", "rgba(0, 0, 0, 1)");
+}
+
+function cardColorDark() {
+
+  $("#cardcolor").css("background-color", "rgb(36, 36, 38)");
+  $("#header").css("color", "rgba(255, 255, 255, 255)");
+  $("#circuitous").css("color", "rgba(255, 255, 255, 255)");
+  $("#circuitous a").css("color", "rgba(255, 255, 255,255)");
+  $("#circuitous p").css("color", "rgba(255, 255, 255,255)");
+  $("#footer a").css("color", "rgba(255, 255, 255, 255)");
 }
 
 
@@ -221,8 +245,39 @@ if (url == 'http://'+window.location.hostname+'/') {
   setTimeout(function(){ changeFavicon('/img/circuitous-16x16.png'); }, 1000);
 }
 
+function time() {
+  var d = new Date();
+  var s = d.getSeconds();
+  //var m = d.getMinutes();
+  var h = d.getHours();
+
+  // console.log(h);
+  // console.log(s);
+
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      if (getCookie("darkmode")=="notset" || getCookie("darkmode")=="auto"){cardColorDark()}
+  } else if (h > 18) {
+      if (getCookie("darkmode")=="notset" || getCookie("darkmode")=="auto"){cardColorDark()}
+  } else if (h < 18 && h < 7) {
+      if (getCookie("darkmode")=="notset" || getCookie("darkmode")=="auto"){cardColorDark()}
+  } else if (h < 18 && h > 7) {
+      if (getCookie("darkmode")=="notset" || getCookie("darkmode")=="auto"){cardColorDefault()}
+  } 
+  /*
+  if (s > 55) {$("#plus").css("color", "rgba(255, 0, 0, 1)") } else { $("#plus").css("color", "") }
+  if (s > 10 && s < 15) {$("#tailcard").css("color", "rgba(255, 0, 0, 1)") } else { $("#tailcard").css("color", "") }
+  */
+}
+
 
 window.onload=function(){
+
+  if (getCookie("darkmode")==""){
+      setCookie ("darkmode","notset",365);
+      cardColorDefault();
+  }
+
+  interval = setInterval(time, 3000);
 
   function detectswipe(el,func) {
     swipe_det = new Object();
