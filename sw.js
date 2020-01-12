fae6d41c8639a70682f35d2bf74d94ac3e057bb8
Glyn Thomas <glynthomas@users.noreply.github.com>
2020-01-12 19:46:30 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,20 +14,35 @@ var filesToCache = [
   '/js/circuitous.js'
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+/* Start the service worker and cache all of the app's content
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
+*/
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll(filesToCache))
+    .catch(error => console.error('💩', error))
+  )
+});
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      if (response) {
+        return response;
+      }
+        return fetch(event.request);
+      }
+    )
   );
 });
