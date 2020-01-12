90d39192ce8aa4cebbf198d29c7ced361e51788d
Glyn Thomas <glynthomas@users.noreply.github.com>
2020-01-12 18:51:13 +0000

    Add files via upload

--- js/circuitous.js
+++ js/circuitous.js
@@ -1,13 +1,4 @@
 
-$.getJSON('/☰/circuitous.json').done(function(data){
-  window.circuitous = data;
-  RandomCircuitous();
-  CountCircuitous();
-  CheckCookie();
-  //console.log(isMobile);
-  if (isMobile == true) {$("#circuitous").removeAttr('onclick')}
-});
-
 var isMobile = false;  //initiate as false
 var obj_key_count = 0;
 var darkstate = null;
@@ -15,13 +6,46 @@ var swipe_det = null;
 var ele = null;
 var ran_key = null;
 var count_key = 0;
-var darkness = null;;
+var darkness = null;
+
+var cardcount = 0;
+var infocount = 0;
+
+
+$.getJSON('/☰/circuitous.json').done(function(data){
+  window.circuitous = data;
+
+  localStorage.setItem('circuitous', JSON.stringify(data));
+
+  cardcount = Object.keys(data).length;
+  console.log('cards ☰ '+cardcount);
+
+  RandomCircuitous();
+  CountCircuitous();
+  CheckCookie();
+
+  //console.log(isMobile);
+  if (isMobile == true) {$("#circuitous").removeAttr('onclick')}
+});
+ 
+$.getJSON('/☰/circuitousinfo.json').done(function(data){
+  window.circuitousinfo = data;
+
+  localStorage.setItem('circuitousinfo', JSON.stringify(data));
+  
+  infocount = Object.keys(data).length;
+  console.log('info ☰ '+infocount);
+
+  //if (isMobile == true) {$("#circuitous").removeAttr('onclick')}
+});
+
 
 // device detection
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
     isMobile = true;
 }
+
 
 function setCookie(cname, cvalue, exdays) {
   var d = new Date();
@@ -56,12 +80,14 @@ function pickRandomCircuitous(){
 }
 
 function pickCircuitousInfo(){
-  $.getJSON('/☰/circuitousinfo.json').done(function(data){
-  window.circuitousinfo = data;
+  //$.getJSON('/☰/circuitousinfo.json').done(function(data){
+  //window.circuitousinfo = data;
+  //localStorage.setItem('circuitousinfo', JSON.stringify(data));
+
   InfoCircuitous();
   setTimeout(function(){ InfoCircuitous(); }, 3000);
   if (isMobile == true) {$("#circuitous").removeAttr('onclick')}
-  });
+  //});
 }
 
 function InfoCircuitous(){
@@ -93,7 +119,7 @@ function InfoCircuitous(){
   //console.log(window.ran_key_select);
   document.getElementById("circuitous").innerHTML = (JSON.stringify(window.selectedcircuitous.circuitousinfo)).replace(/\"/g, "").replace(/\"/g, "");
   document.getElementById("cowabunga").innerHTML = window.ran_key_select;
-  console.info(ran_key_select + ' ' + JSON.stringify(window.selectedcircuitous.circuitousinfo).replace(/\"/g, "").replace(/\"/g, ""));
+  console.info('card ☰ '+ran_key_select + ' ' + JSON.stringify(window.selectedcircuitous.circuitousinfo).replace(/\"/g, "").replace(/\"/g, ""));
   
   obj_key_count = obj_key_count + 1;
 }
@@ -105,7 +131,7 @@ function RandomCircuitous(){
   window.selectedcircuitous = window.circuitous[ran_key_select];
   document.getElementById("circuitous").innerHTML = (JSON.stringify(window.selectedcircuitous.circuitous)).replace(/\"/g, "").replace(/\"/g, "");
   document.getElementById("cowabunga").innerHTML = window.ran_key_select;
-  console.info(ran_key_select + ' ' + JSON.stringify(window.selectedcircuitous.circuitous).replace(/\"/g, "").replace(/\"/g, ""));
+  console.info('card ☰ '+ran_key_select + ' ' + JSON.stringify(window.selectedcircuitous.circuitous).replace(/\"/g, "").replace(/\"/g, ""));
 }
 
 function CountCircuitous(){
@@ -208,7 +234,6 @@ function cardColor(backcolor){
       $("#circuitous p").css("color", "rgba(0, 0, 0, 1)");
       $("#footer a").css("color", "rgba(0, 0, 0, 1)");
 
-      //setCookie ("darkmode","disable",365);
       setCookie ("darkmode","auto",365);
       darkTime(true);
   }
@@ -245,11 +270,36 @@ if (url == 'http://'+window.location.hostname+'/') {
 }
 
 
-function updateJson() {
+function onlineJson() {
   $.getJSON('/☰/circuitous.json').done(function(data){
   	window.circuitous = data;
+    var netcount = Object.keys(data).length;
+    //console.log(netcount);
+    if (netcount != cardcount) { // new content detected
+      if ('serviceWorker' in navigator) {
+        localStorage.setItem('circuitous', JSON.stringify(data));
+        window.circuitous = JSON.parse(localStorage.getItem('circuitous'));
+        //console.log('update window.circuitous data');
+      }
+    }
+    cardcount = netcount;
   	//console.log(isMobile);
   	if (isMobile == true) {$("#circuitous").removeAttr('onclick')}
+  });
+
+  $.getJSON('/☰/circuitousinfo.json').done(function(data){
+    window.circuitousinfo = data;
+    var infocount = Object.keys(data).length;
+    //console.log(infocount);
+    if (infocount != infocount) { // new content detected
+      if ('serviceWorker' in navigator) {
+        localStorage.setItem('circuitousinfo', JSON.stringify(data));
+        window.circuitousinfo = JSON.parse(localStorage.getItem('circuitousinfo'));
+        //console.log('update window.circuitousinfo data');
+      }
+    }
+
+  if (isMobile == true) {$("#circuitous").removeAttr('onclick')}
   });
 }
 
@@ -257,9 +307,6 @@ function updateJson() {
 function darkTime(darkstate) {
   
 //  darkTime : true  * start || false * stop
-
-  //console.log('inside darkTime');
-  //darkstate = wstate;
 
     var newWorker = function (funcObj) {
     var blobURL = URL.createObjectURL(new Blob(['(',
@@ -286,10 +333,6 @@ function darkTime(darkstate) {
     });
 
     w1.onmessage = function (event) { // 3000
-
-      //console.log(event.data);
-      //console.log(h);
-      //console.log(s);
       
     if (darkstate == false){
         //console.log('darktime false!');
@@ -313,10 +356,10 @@ function darkTime(darkstate) {
       	case (40): // every 30 seconds
       	case (50): // every 30 seconds
     		if (navigator.onLine) {
-    			console.log('online - check for new content -- every 30 seconds!');
-    			updateJson();
+    			console.log('online ☰ auto 30s update check');
+    			onlineJson();
     		} else {
-    			console.log('offline');
+    			console.log('offline ☰ store * cache');
     		}
     	break;
     }
@@ -358,11 +401,10 @@ function darkTime(darkstate) {
         if (getCookie("darkmode")=="notset"){cardColorDark()}
         if (getCookie("darkmode")=="auto"){cardColorDark()}
     }
-
       //console.log(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
-
     }
 }
+
 
 window.onload=function(){
 
@@ -373,10 +415,7 @@ window.onload=function(){
 
   darkTime(true);
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
+  if ('serviceWorker' in navigator) {navigator.serviceWorker.register('./sw.js');}
 
   function detectswipe(el,func) {
     swipe_det = new Object();
